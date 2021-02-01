a69b79ef36757e5b69650a51ab84700b22c90f9f
Philip Bilton <pbilton85@gmail.com>
2021-02-01 14:04:55 +0000

    testing clue selection

--- clues.py
+++ clues.py
@@ -1,13 +1,31 @@
+import random
 class clues:
 
     def __init__(self, answer, maxNumber):
         self.answer = answer
         self.maxNumber = maxNumber
-        self.numberDividables = self.numberDividable()
+        self.numberDividables = []
+        self.isNumberDividable()
+        self.clueChoice = ['Even', 'Higher', 'Dividable']
         return
 
-    def isNumberHigher(self, userNumber):
-        if userNumber > self.answer:
+    def selectClue(self, userGuess):
+        clue = random.choice(self.clueChoice)
+        if clue == 'Even':
+            self.clueChoice.remove('Even')
+            return self.isNumberEven()
+        elif clue == 'Higher':
+            return self.isNumberHigher(userGuess)
+        else:
+            if self.getDividable() != "No more multiples remain":
+                return self.getDividable()
+            else:
+                self.clueChoice.remove('Dividable')
+                #return self.selectClue(userGuess)
+
+        
+    def isNumberHigher(self, userGuess):
+        if userGuess < self.answer:
             return "Higher"
         else:
             return "Lower"
@@ -18,6 +36,20 @@ class clues:
         else:
             return "It is an odd number"
 
-    def numberDividable(self):    
+    def isNumberDividable(self):
+        for number in range(1,self.answer+1):
+            if self.answer % number == 0:
+                self.numberDividables.append(number)    
         return
+    
+    def getDividable(self):
+        if len(self.numberDividables) != 0:
+            dividable = random.choice(self.numberDividables)
+            self.numberDividables.remove(dividable)
+            return "Number is dividable by " + str(dividable)
+        else:
+            return "No more multiples remain"
+    
+    
+
 
---
+++ testClueTemp.py
@@ -0,0 +1,23 @@
+from clues import clues
+
+def main():
+    clue = clues(6,20)
+    # print(clue.isNumberEven())
+    # print(clue.isNumberHigher(4))
+    # print("")
+
+    # print(clue.getDividable())
+    # print(clue.getDividable())
+    # print(clue.getDividable())
+    # print(clue.getDividable())
+    # print(clue.getDividable())
+    # print("")
+    # print(clue.selectClue(5))
+
+    print(clue.selectClue(2))
+    print(clue.selectClue(8))
+    print(clue.selectClue(3))
+    print(clue.selectClue(1))
+
+
+main()
